537054f4c84e93d63c12c538b583ae538ce33b72
Soland <ranaee2000@gmail.com>
2023-01-07 21:18:35 +0330

    Move Disk Function completed

--- script.js
+++ script.js
@@ -15,3 +15,53 @@ function ExHanoi(A, B, C, n) {
     Hanoi(B, A, C, 3 * n - 1);
   }
 }
+
+function createDisks(rod, rodDisks, diskCount) {
+  rod.children(".disk").remove();
+  for (let i = 0; i < diskCount; i++) {
+    let disk = $("<div></div>");
+    disk.addClass("disk");
+    disk.css("background-color", getRandomColor());
+    disk.css("width", 90 - i * 10);
+    disk.css("bottom", i * 10);
+    rod.append(disk);
+    rodDisks.push(disk);
+  }
+}
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function moveDisk(disk, rod) {
+    let bottom = disk.css("bottom");
+    disk.animate(
+      {
+        bottom: "100%",
+      },
+      {
+        duration: 1000,
+      }
+    );
+    disk.animate(
+      {
+        left: rod.offset().left,
+      },
+      {
+        duration: 1000,
+      }
+    );
+    disk.animate(
+      {
+        bottom: bottom,
+      },
+      {
+        duration: 1000,
+      }
+    );
+  }
